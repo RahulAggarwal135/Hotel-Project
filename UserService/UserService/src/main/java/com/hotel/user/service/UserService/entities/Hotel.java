218b14808cf710158d0102859d0195ae4c75428d
Rahul Aggarwal <rahulaggarwal135@hotmail.com>
2023-09-01 18:45:20 +0530

    Inter service Communication implemented

--- UserService/UserService/src/main/java/com/hotel/user/service/UserService/entities/Hotel.java
+++ UserService/UserService/src/main/java/com/hotel/user/service/UserService/entities/Hotel.java
@@ -4,19 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Rating {
-
-
-    private String ratingId;
-    private String userId;
-    private String hotelId;
-    private int rating;
-    private String feedback;
-    private Hotel hotel;
-
+public class Hotel {
+    private String id;
+    String name;
+    String location;
+    String about;
 }
--- UserService/UserService/src/main/java/com/hotel/user/service/UserService/services/impl/UserServiceImpl.java
+++ UserService/UserService/src/main/java/com/hotel/user/service/UserService/services/impl/UserServiceImpl.java
@@ -1,19 +1,29 @@
 package com.hotel.user.service.UserService.services.impl;
 
+import com.hotel.user.service.UserService.entities.Hotel;
+import com.hotel.user.service.UserService.entities.Rating;
 import com.hotel.user.service.UserService.entities.User;
 import com.hotel.user.service.UserService.services.exceptions.ResourceNotFoundException;
 import com.hotel.user.service.UserService.repositories.UserRepository;
 import com.hotel.user.service.UserService.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    RestTemplate restTemplate;
 
     @Override
     public User saveUser(User user) {
@@ -27,7 +37,24 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(String userId) {
-        return userRepository.findById(userId).orElseThrow(()-> new ResourceNotFoundException("User is not found with Id: " + userId));
+        User user= userRepository.findById(userId).orElseThrow(()-> new ResourceNotFoundException("User is not found with Id: " + userId));
+
+        // ratings/user/{userId}
+
+
+        Rating[] ratingArrayList = restTemplate.getForObject("http://localhost:8083/ratings/user/"+userId, Rating[].class);
+
+        List<Rating> ratingList = Arrays.stream(ratingArrayList).toList();
+
+        List<Rating> ratings = ratingList.stream().map(rating -> {
+            ResponseEntity<Hotel> hotelResponseEntity = restTemplate.getForEntity("http://localhost:8082/hotel/"+rating.getHotelId(), Hotel.class);
+            Hotel hotel = hotelResponseEntity.getBody();
+            rating.setHotel(hotel);
+            return rating;
+        }).collect(Collectors.toList());
+
+        user.setRatings(ratings);
+        return user;
     }
 
     @Override
